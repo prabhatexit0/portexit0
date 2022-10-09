77d9019e461506536539ddaa4ff60ebcfff025a1
p-exit0 <prabhatexit0@gmail.com>
2022-10-10 01:47:47 +0530

    sec fix

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -4,10 +4,10 @@ export default function Contact() {
 	return <div className="font-mono w-full border-zinc-900 border-y-2 p-4 absolute bottom-0 flex gap-2 justify-center">
 		<p>Looking for a versatile developer?</p> 
 		<div className="flex gap-2 items-center">
-			<a target="_blank" href="https://www.linkedin.com/in/prabhatexit0/">
+			<a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/prabhatexit0/">
 				<AiOutlineGithub size={23}/>
 			</a>
-			<a target="_blank" href="https://github.com/prabhatexit0">
+			<a target="_blank" rel="noreferrer" href="https://github.com/prabhatexit0">
 				<AiFillLinkedin size={23}/>
 			</a>
 		</div>
