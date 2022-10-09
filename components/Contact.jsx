aa10bb616be948023370525b535c5abfca2b7dc2
p-exit0 <prabhatexit0@gmail.com>
2022-10-10 01:10:15 +0530

    contact footer added

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,5 +1,11 @@
+import { AiOutlineGithub, AiFillLinkedin } from 'react-icons/ai'
+
 export default function Contact() {
-	return <div className="w-full border-zinc-900 border-y-2 p-4 absolute bottom-0">
-			
+	return <div className="font-mono w-full border-zinc-900 border-y-2 p-4 absolute bottom-0 flex gap-2 justify-center">
+		<p>Looking for a versatile developer?</p> 
+		<div className="flex gap-2 items-center">
+			<AiOutlineGithub size={23}/>
+			<AiFillLinkedin size={23}/>
+		</div>
 	</div>
 }
