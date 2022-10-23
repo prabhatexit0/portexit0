a43520eb38ed0a49ea6047bfa98b77cb1bada310
p-exit0 <prabhatexit0@gmail.com>
2022-10-24 03:35:02 +0530

    * added vim toggle * added red color for error

--- components/games/Executor.jsx
+++ components/games/Executor.jsx
@@ -1,7 +1,8 @@
-import { useState, useRef } from "react"
-import ReactCodeMirror, { basicSetup } from "@uiw/react-codemirror";
+import { useState } from "react"
+import ReactCodeMirror, { basicSetup  } from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import {dracula} from "@uiw/codemirror-themes-all";
+import { vim } from '@replit/codemirror-vim'
 
 const URL = 'https://rocky-dawn-53721.herokuapp.com/run'
 
@@ -9,6 +10,15 @@ export default function Executor() {
   const [result, setResult] = useState("")
   const [isLoading, setIsLoading] = useState(false)
   const [codeString, setCodeString] = useState("// Work in progress, might not work!")
+  const [isVim, setIsVim] = useState(false)
+  const [codeExtensions, setCodeExtensions] = useState([
+    javascript(),
+    basicSetup({
+      autocompletion: true,
+      foldGutter: true
+    })
+  ])
+  const [codeError, setCodeError] = useState(false)
 
   const handleFileCreation = async () => {
     setIsLoading(true)
@@ -23,10 +33,23 @@ export default function Executor() {
 
     if (responseData.result !== undefined) {
       setResult(responseData.result)
+      setCodeError(false)
+    } else {
+      setCodeError(true)
+      setResult("ERROR!!")
     }
   }
 
-  return <div className="font-mono flex flex-col gap-2 executor">
+  const handleVimToggle = () => {
+    if(isVim) setCodeExtensions(codeExtensions.slice(1))
+    else setCodeExtensions([vim(), ...codeExtensions])
+    setIsVim(c => !c)
+  }
+
+  return <div className="font-mono flex flex-col gap-2 executor relative">
+    <button onClick={handleVimToggle} className={`px-2 font-bold w-max 
+      ${ isVim ? "bg-yellow-300 text-black" : "bg-gray-700 text-white" } 
+      text-white absolute right-0 top-0`}>Vim</button>
     <h1 className="font-bold text-3xl">Execute JS</h1>
     <div className="relative">
       <ReactCodeMirror
@@ -34,21 +57,19 @@ export default function Executor() {
         onChange={value => setCodeString(value)}
         className="text-black max-h-40 overflow-auto"
         options={{
-          mode: "jsx",
+          mode: "js",
         }}
         theme={dracula}
         height="200px"
-        extensions={[
-          javascript(),
-          basicSetup({
-            autocompletion: true,
-            foldGutter: true
-          })
-        ]}
+        extensions={codeExtensions}
       />
-      <button onClick={handleFileCreation} className="p-2 font-bold w-max bg-[#295]
+      <button onClick={handleFileCreation} className="p-1 px-2 font-bold w-max bg-[#295]
         text-white absolute bottom-0 right-0 m-2">Run</button>
     </div>
-    <div className="max-h-28 overflow-auto font-bold">Result: {!isLoading ? <pre className="font-normal">{result}</pre> : "Loading..."} </div>
+    <div className="max-h-28 overflow-auto font-bold">
+      Result: {!isLoading ?
+      <pre className={`font-normal ${codeError ? "text-red-500" : "text-white"}`}>{result} </pre> :
+      "Loading..."}
+    </div>
   </div>
 }
