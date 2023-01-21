0190c76361decdfbdaf05821caab12fb4022ccb9
prabhatexit0 <prabhatexit0@gmail.com>
2023-01-22 02:38:06 +0530

    minor font size fix

--- components/widgets/Introduction.jsx
+++ components/widgets/Introduction.jsx
@@ -3,7 +3,7 @@ const RESUME_LINK = process.env.NEXT_PUBLIC_RESUME_LINK
 export default function Introduction() {
   return (
     <AboutContainer>
-      <h1 className="text-3xl font-bold text-white font-mono text-gray-300">Prabhat Sachdeva</h1>
+      <h1 className="text-2xl font-bold text-white font-mono text-gray-300">Prabhat Sachdeva</h1>
       <p>Final year undergraduate student with expertise in JavaScript, TypeScript, React.js and related frameworks.
         I have experience in solving LeetCode problems and have an interest in WebAssembly and Compiler Design.
         For more information, please refer to my <a className="font-bold cursor-pointer border-[1px] px-1 border-gray-700 rounded-md hover:bg-gray-900" href={RESUME_LINK}>resume</a>.</p>
